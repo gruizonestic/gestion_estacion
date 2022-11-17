6ed9ad7f51a3b7e20a7b7ace30a86f62e0941f5e
gruizonestic <guillermo.ruiz@onestic.com>
2022-11-17 11:49:24 +0100

    api luces terminada

--- servicios_propios/backend/main.py
+++ servicios_propios/backend/main.py
@@ -1,9 +1,13 @@
+# Codigo de prueba del backend.
+# Escribe el fichero /var/backend/test.txt
+# cada vez que se ejecuta un contenedor.
+
 if __name__ == "__main__":
     write = 0
     while True:
         print("Esto sería el código del backend")
         if write == 0:
             f = open("/var/backend/test.txt", "a")
-            f.write("He escrito algo")
+            f.write("He escrito algo\n")
             f.close()
             write = 1
