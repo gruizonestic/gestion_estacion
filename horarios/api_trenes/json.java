ca67382ba5708a5a5ccb69af58c2c913f8726fcc
Jordi Ros Mayor <jordi.ros@onestic.com>
2022-11-21 15:35:33 +0100

    Comentarios de cada uno de los servicios

---
+++ horarios/api_trenes/json.java
@@ -0,0 +1,8 @@
+package horarios.api_trenes;
+
+public class json{
+    public static void main(String[] args) throws InterruptedException {
+        System.out.println("Aquí se ejecuta el código de la API JSON");
+        while(true) {Thread.sleep(1000);}
+    }
+}
