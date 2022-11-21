50724a67f527d01a2cc71ac6d08fd2251fa75e68
Jordi Ros Mayor <jordi.ros@onestic.com>
2022-11-21 11:41:22 +0100

    Backend y frontend funcionando en horarios

--- horarios/backend/back.js
+++ horarios/backend/back.js
@@ -1,2 +1,8 @@
+function sleep(s) {
+    return new Promise(resolve => setTimeout(resolve,s))
+}
+
 console.log('Aquí funciona el backend de la aplicación de horarios')
-while(true) {}
+while(true) {
+    sleep(1000)
+}
