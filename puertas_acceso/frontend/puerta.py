edf7b04061d11371831a4f9ad27baf2a5d8fa0bc
Guillermo <guillermo.navio@onestic.com>
2022-11-22 13:41:32 +0100

    puertas_acceso ya no se cierran

--- puertas_acceso/frontend/puerta.py
+++ puertas_acceso/frontend/puerta.py
@@ -1,8 +1,21 @@
 import requests
+import random
+import time
+from requests.adapters import HTTPAdapter
+from urllib3.util.retry import Retry
 
-url = 'http://puerta.backend1:5207/validar'
-myobj = {'Numero' : '1234'}
+backend_url = ['http://puerta.backend1:5207/validar', 'http://puerta.backend2:5207/validar', 'http://puerta.backend3:5207/validar']
+billete = {'Numero' : ''}
 
-x = requests.post(url, json = myobj)
+while True:
+    numero_billete = random.randint(1, 99999)
+    indice_backend = random.randint(0, 2)
+    pasa_alguien = random.randint(1, 7)
+    billete['Numero'] = str(numero_billete)
+    time.sleep(pasa_alguien)
 
-print(x.text)
+    try:
+        x = requests.post(backend_url[indice_backend], json = billete, verify=False, timeout=10)
+        print(x.text)
+    except:
+        x = requests.post(backend_url[indice_backend], json = billete, verify=False, timeout=10)
