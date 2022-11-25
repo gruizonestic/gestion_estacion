345f84e03260720eb1fa5a22398248ec06dd89f2
gruizonestic <guillermo.ruiz@onestic.com>
2022-11-25 11:44:03 +0100

    back.js fixed

--- horarios/backend/back.js
+++ horarios/backend/back.js
@@ -1,8 +1,13 @@
-function sleep(s) {
-    return new Promise(resolve => setTimeout(resolve,s))
+function logEvery2Seconds(i) {
+    setTimeout(() => {
+        console.log('Infinite Loop Test n:', i);
+        logEvery2Seconds(++i);
+    }, 2000)
 }
 
-console.log('Aquí funciona el backend de la aplicación de horarios')
-while(true) {
-    sleep(1000)
-}
+logEvery2Seconds(0);
+
+let i = 0;
+setInterval(() => {
+    console.log('Infinite Loop Test interval n:', i++);
+}, 2000)
